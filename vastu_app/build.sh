08e45a2d496d45ea3417201d1016f8c5af69fffc
hetvi004 <gohelhetvi18@gmail.com>
2026-04-28 02:23:06 +0530

    auto create superuser on deploy

--- vastu_app/build.sh
+++ vastu_app/build.sh
@@ -2,3 +2,13 @@
 pip install -r requirements.txt
 python vastu_app/manage.py collectstatic --no-input
 python vastu_app/manage.py migrate --run-syncdb
+
+python vastu_app/manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'gohelhetvi47@gmail.com', 'Hetvi@1807')
+    print('Superuser created!')
+else:
+    print('Superuser already exists.')
+"
