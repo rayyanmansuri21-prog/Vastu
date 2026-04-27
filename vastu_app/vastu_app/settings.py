1327d50d89bdb079437a4216a07a699099b9dba5
hetvi004 <gohelhetvi18@gmail.com>
2026-04-28 01:36:20 +0530

    fix static files for production

--- vastu_app/vastu_app/settings.py
+++ vastu_app/vastu_app/settings.py
@@ -139,6 +139,7 @@ STATIC_ROOT = BASE_DIR/"staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
