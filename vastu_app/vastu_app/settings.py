c14a854c4ae0cc19ac65f8a6453f18c98d909884
hetvi004 <gohelhetvi18@gmail.com>
2026-04-28 01:07:26 +0530

    switch to sqlite database

--- vastu_app/vastu_app/settings.py
+++ vastu_app/vastu_app/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = "django-insecure-51nrr85!+5tktt66iti!*5z#f%p-i29*l(#!qw#t+))18n@da*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -82,18 +82,23 @@ WSGI_APPLICATION = "vastu_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+#
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": 'vastu_db',
+#         "USER": "root",
+#         "PASSWORD": "sql@12345",
+#         "HOST": "localhost",
+#         "PORT": "3306",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": 'vastu_db',
-        "USER": "root",
-        "PASSWORD": "sql@12345",
-        "HOST": "localhost",
-        "PORT": "3306",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
