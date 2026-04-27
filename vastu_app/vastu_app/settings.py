44e3d7d0e9a3c43453793a2bc581cb877f269498
hetvi004 <gohelhetvi18@gmail.com>
2026-04-28 01:49:40 +0530

    add whitenoise for static files

--- vastu_app/vastu_app/settings.py
+++ vastu_app/vastu_app/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "vastu_app.urls"
@@ -139,7 +140,7 @@ STATIC_ROOT = BASE_DIR/"staticfiles"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
